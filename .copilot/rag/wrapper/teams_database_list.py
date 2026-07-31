2176e648d58ba3868c9f2e4ba598626f0a26c5b4
harilos <sparkingkipple11@gmail.com>
2026-07-31 11:54:59 +0900

    feat: label GitLab Wiki sources in DB list

--- .copilot/rag/wrapper/teams_database_list.py
+++ .copilot/rag/wrapper/teams_database_list.py
@@ -4,6 +4,7 @@ from . import database_list as _database_list
 
 
 _database_list.TYPE_LABELS["teams"] = "Microsoft Teams"
+_database_list.TYPE_LABELS["gitlab_wiki"] = "GitLab Wiki"
 main = _database_list.main
 
 
