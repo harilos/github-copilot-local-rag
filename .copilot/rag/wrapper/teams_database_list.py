8b9bd925a1b7a585b6d7ff4da1f475ba9ee2ac07
harilos <sparkingkipple11@gmail.com>
2026-08-01 00:15:13 +0900

    ui: align Git source type labels

--- .copilot/rag/wrapper/teams_database_list.py
+++ .copilot/rag/wrapper/teams_database_list.py
@@ -5,8 +5,8 @@ from . import database_list as _database_list
 
 _database_list.TYPE_LABELS["github"] = "GitHub"
 _database_list.TYPE_LABELS["gitlab"] = "GitLab"
-_database_list.TYPE_LABELS["azure_devops"] = "Azure DevOps"
-_database_list.TYPE_LABELS["git"] = "その他のGit"
+_database_list.TYPE_LABELS["azure-devops"] = "Azure DevOps"
+_database_list.TYPE_LABELS["other-git"] = "その他のGit"
 _database_list.TYPE_LABELS["teams"] = "Microsoft Teams"
 _database_list.TYPE_LABELS["gitlab_wiki"] = "GitLab Wiki"
 main = _database_list.main
